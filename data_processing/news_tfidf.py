d84aead009b9216ce87f9eb526e02c37ed615735
UladKasach <nappa300@gmail.com>
2018-04-06 13:41:34 -0400

    calculate tfidf+svd for each news title as a document + define news ids for vol_day

---
+++ data_processing/news_tfidf.py
@@ -0,0 +1,106 @@
+'''
+    normalization:
+    1. load all data into pandas
+    2. cast "date" field into readable date
+    3. extract "catagory" from url of reuters news
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates # used to parse x-axis values as dates
+import csv
+import argparse
+import pandas as pd
+import datetime as dt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+
+## methods
+def extract_date(timestamp):
+    date = timestamp[0:10];
+    return date;
+def reduce_to_requested_date_range(data, date_to_start, date_to_end):
+    ## reduce date range to requested range
+    data = data[data.Date >= date_to_start]
+    data = data[data.Date <= date_to_end];
+    return data;
+
+## parse arguments
+parser = argparse.ArgumentParser();
+parser.add_argument('path_news', help='Path to Normalized News Data');
+parser.add_argument('-d', '--dimensionality', metavar="D", type=int, default=300);
+parser.add_argument('-m', '--min_freq', metavar="D", type=int, default=3);
+parser.add_argument('-r', '--readable_output', action='store_true');
+parser.add_argument('-s', '--start_year', metavar='YYYY', default = "2018");
+parser.add_argument('-e', '--end_year', metavar='YYYY', default="2018");
+args = parser.parse_args();
+
+## get news data
+print("loading data");
+data = pd.read_csv(args.path_news);
+data = data.drop('URL', 1)
+data = data.drop('Catagory', 1)
+
+## extract Date  and Time from Timestamp
+print("extracting `Date` field")
+data["Date"] = data["TimeStamp"].apply(lambda x: extract_date(x));
+
+## filter news by date
+date_to_start = (args.start_year+"-01-01"); # first day of the year
+date_to_end = (args.end_year+"-12-31"); # last day of the year
+data = reduce_to_requested_date_range(data, date_to_start, date_to_end);
+
+# documents : 2730187 - 2014
+# documents : 106438 - 2018
+
+## remove rows with null
+orig_len = len(data.index)
+data = data.dropna() # if any col is null in a row, remove it
+filt_len = len(data.index);
+print("dates filtered out: " + str(orig_len - filt_len));
+
+## reset index so that now index of dataframe correlates with index of corpus output in next part
+data = data.reset_index(drop=True)
+
+## extract corpus from data
+corpus = data["Title"].tolist();
+print("documents : " + str(len(corpus)));
+
+## calculate tf-idf for corpus
+tfidf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = args.min_freq, stop_words = 'english'); # dont consider words that occur less than 3 times
+print("fitting tfidf to corpus");
+tfidf_matrix =  tfidf.fit_transform(corpus)
+feature_names = tfidf.get_feature_names()
+print("features : " + str(len(feature_names)))
+# features : 114790
+# features : 829147 p 2o16
+
+
+## reduce dimensionality with SVD (- this results in us running LSA since LSA = TF-IDF + SVD)
+print("reducing dimensionality");
+svd = TruncatedSVD(n_components=args.dimensionality, n_iter=7);
+lsa_matrix = svd.fit_transform(tfidf_matrix)
+
+## append the vector of each row to the data
+def grab_vector_for_row(row):
+    vector = (lsa_matrix[row.name, :]);
+    vector = list(vector);
+    #print(vector);
+    return vector;
+data["Vector"] = data.apply(grab_vector_for_row, axis = 1);
+
+## drop the title from dataframe
+data = data.drop("Title", axis=1);
+
+## output the vectorized data
+file_name = args.path_news.split("/")[-1];
+dir_path = "/".join(args.path_news.split("/")[:-1]);
+name_parts = file_name.split(".");
+name_parts.insert(-1, "tfidf.svd.dim_"+str(args.dimensionality));
+name_parts = name_parts[:-1]; # drop extension
+file_name = ".".join(name_parts);
+file_path = dir_path + "/" + file_name;
+
+data.to_hdf(file_path + ".hdf", "data", mode="w");
+if(args.readable_output):
+    print("readable output");
+    data.to_csv(file_path  + ".csv", index=False);
---
+++ data_processing/vol_day_news_ids.py
@@ -0,0 +1,104 @@
+'''
+    given a dataframe with news data (including an id for each news article),
+     generate a dataframe that lists by id each news article in the "News" column
+'''
+import argparse
+import numpy as np
+import pandas as pd
+import datetime as dt
+import pandas_util.parallel as pandas_parallel
+
+## methods
+def convert_to_date_float(date_string):
+    return dt.datetime.strptime(date_string,'%Y-%m-%d').date()
+def reduce_to_requested_date_range(data, date_to_start, date_to_end):
+    ## reduce date range to requested range
+    data = data[data.Date >= date_to_start]
+    data = data[data.Date <= date_to_end];
+    return data;
+
+## parse arguments
+parser = argparse.ArgumentParser();
+parser.add_argument('-l', '--path_labels', help='Path to Volatility Labels');
+parser.add_argument('-n', '--path_news', metavar='PATH', help="Path to News Data")
+parser.add_argument('-s', '--start_year', metavar='YYYY', default = "2018");
+parser.add_argument('-e', '--end_year', metavar='YYYY', default="2018");
+parser.add_argument('-w', '--workers', metavar="D", type=int, default=1)
+parser.add_argument('-p', '--period', metavar='DAYS', type=int, default=2);
+parser.add_argument('-r', '--readable_output', action='store_true');
+args = parser.parse_args();
+
+## get data - filter out by date as well
+date_to_start = (args.start_year+"-01-01"); # first day of the year
+date_to_end = (args.end_year+"-12-31"); # last day of the year
+print("reading vol_data");
+vol_data = pd.read_csv(args.path_labels);
+vol_data = reduce_to_requested_date_range(vol_data, date_to_start, date_to_end);
+print("reading news data");
+news_data = pd.read_hdf(args.path_news, 'data');
+news_data["Date"] = news_data["TimeStamp"].apply(lambda timestamp: timestamp[:10]); ## extract date from timestamp
+news_data = reduce_to_requested_date_range(news_data, date_to_start, date_to_end);
+
+## methods
+def extract_full_ids(day, days_to_subtract, date_to_start, date_to_end):
+    ## extract start and end date from
+    target_date = convert_to_date_float(day);
+    start_date = target_date - dt.timedelta(days=days_to_subtract)
+    end_date = target_date - dt.timedelta(days=1); # day before, so we dont get news from the day we're predicting - only prior
+
+    ## validate the date range
+    if(start_date < date_to_start): return None; # start date is not after date to start - we wont have all data
+    if(end_date > date_to_end): return None; # end date is not after date to end - we wont have all data
+
+    # retreive all elements within this range
+    relevant_news = reduce_to_requested_date_range(news_data, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'));
+
+    # retreive all tokens from the relevant articles
+    full_ids = relevant_news.index.tolist();
+
+    ## increment counter and output result for this thread
+    if(this_day % 10 == 0):
+        print("on day " + str(this_day) + " out of " + str(total_days));
+    global this_day;
+    this_day += 1;
+
+    ## return result
+    return full_ids;
+
+
+## for each day in volatility data, create a bag of words dict for words/frequencies found in that period
+print("extracting full tokens for range")
+data = vol_data; # rename dataframe now that we're going to be appending tokens for each date
+date_to_start = convert_to_date_float(date_to_start);
+date_to_end = convert_to_date_float(date_to_end);
+days_to_subtract = args.period;
+total_days = len(data.index);
+this_day = 0;
+def applicator(date):
+    result = extract_full_ids(date, days_to_subtract, date_to_start, date_to_end); ## get full_tokens
+    return result; ## return result
+if(args.workers == 1):
+    tokens_result = data["Date"].apply(lambda date: applicator(date))
+else:
+    tokens_result = pandas_parallel.apply_by_multiprocessing(data["Date"], applicator, workers=args.workers);
+data["News"] = tokens_result;
+
+## remove NaN values
+print("filtering out NaN values")
+orig_len = len(data.index)
+data = data.dropna() # if any col is null in a row, remove it
+data = data.reset_index();
+filt_len = len(data.index);
+print("dates filtered out: " + str(orig_len - filt_len))
+
+## reduce data
+data = pd.DataFrame(data[["Date", "Volatility", "Label", "News"]]);
+
+## output the normalized data
+file_name = args.path_news.split("/")[-1];
+file_name_no_ext = ".".join(file_name.split(".")[:-1])
+file_path = "../data/vol_day/indicies."+str(args.period) + "_days_before." + args.start_year + "_to_" + args.end_year+".for_" + file_name_no_ext;
+data.to_hdf(file_path+".hdf", "data", mode='w');
+if(args.readable_output):
+    print("readable output");
+    data.to_csv(file_path  + ".csv", index=False);
